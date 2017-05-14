fbe8d4a73688ea45749cc4a2b8b4f0f2b9ea7b29
melissa135 <1071560244@qq.com>
2017-05-14 01:38:38 -0700

    Update and rename create_train_test.py to create_samples.py

--- create_samples.py
+++ create_samples.py
@@ -4,7 +4,7 @@ import random
 from PIL import Image
 from my_image_folder import is_image_file
 
-def oversample(wind): # return how many to oversample (according to wind level)
+def oversample_num(wind): # return how many to oversample (according to wind level)
     if wind < 60:
         return 1
     if wind < 80:
@@ -13,26 +13,35 @@ def oversample(wind): # return how many to oversample (according to wind level)
         return 1 + random.randint(0,2)
     return 1 + random.randint(0,10)
 
-def create_file(f,fname,f_root,count): # oversample specific copies
+def save_file(f,fname,f_root): # oversample specific copies
     wind = int(fname.split('_')[2])
-    cps = oversample(wind)
+    
+    if oversample :
+        cps = oversample_num(wind)
+    else :
+        cps = 1
+
+    global count
     count = count + cps
+    
     for i in range(0,cps):
         temp = fname.split('.')
         temp[0] = temp[0]+'_'+str(i)
         new_fname = temp[0]+'.'+temp[1] # append a copy-number to filename
         f.save(f_root+new_fname)
-    return count
 
 def if_match(f1,f2): # match : f1 is 6-hour earlier than f2, and they are same ty
     tname1 = f1.split('_')
     tname2 = f2.split('_')
+    
     if tname1[0]!=tname2[0]:
 	return False
+    
     date1 = tname1[1]
     date2 = tname2[1]
     h1 = date1[len(date1)-1]
     h2 = date2[len(date2)-1]
+    
     if (h1=='0' and h2=='6')or(h1=='6' and h2=='2')or(h1=='2' and h2=='8')or(h1=='8' and h2=='0'):
 	return True
     else :
@@ -52,6 +61,26 @@ def merge_pics(p1,p2): # red-channel:6-hour earlier pic, green-channel:current p
     im = Image.merge('RGB',(r,g,b))
     return im
 
+def create_sample(source_dir,fname_1,fname_2,target_dir): # combine two raw images to a legal sample for our CNN
+    
+    complete_fname_1 = os.path.join(root,fnames[i-1])
+    complete_fname_2 = os.path.join(root,fnames[i])
+
+    if not(is_image_file(complete_fname_1) and is_image_file(complete_fname_2)):
+	return 'Not image file: ',complete_fname_1,complete_fname_2
+
+    if not if_match(fname_1,fname_2):
+        return 'Two images are not matched: ',fname_1,fname_2
+	    
+    img_1 = Image.open(complete_fname_1)
+    img_2 = Image.open(complete_fname_2)
+    
+    img_1 = cut_pics(img_1)
+    img_2 = cut_pics(img_2)
+    im = merge_pics(img_1,img_2)
+
+    save_file(im,fname_2,target_dir)
+
 
 if __name__ == '__main__':
     
@@ -61,40 +90,47 @@ if __name__ == '__main__':
     train_root = path_ + '/train_set/'
     if not os.path.exists(train_root):
 	os.mkdir(train_root)
+	
     test_root = path_ + '/test_set/'
     if not os.path.exists(test_root):
 	os.mkdir(test_root)
 
-    global count_train,count_test
-    count_train,count_test = 0,0
+    global count, oversample
+    count = 0
+    oversample = True
 
     for root, _, fnames in sorted(os.walk(raw_dir)):
 
 	fnames = sorted(fnames)
 	boundary = int(len(fnames)*0.8) # 80% samples as train set and 20% samples as test set
 
-	for i in range(1,len(fnames)):
-	    if not is_image_file(fnames[i-1]) or not is_image_file(fnames[i]):
-		continue
+	for i in range(1,boundary): # create train set
 	    
-	    img_1 = Image.open(os.path.join(root,fnames[i-1]))
-	    img = Image.open(os.path.join(root,fnames[i]))
-	    
-	    if if_match(fnames[i-1],fnames[i]):
-		img_1 = cut_pics(img_1)
-		img = cut_pics(img)
-		im = merge_pics(img_1,img)
-
-		if i <= boundary :
-                    count_train = create_file(im,fnames[i],train_root,count_train)
-                else :
-                    count_test = create_file(im,fnames[i],test_root,count_test)
+	    info = create_sample(root,fnames[i-1],fnames[i],train_root)
+	    if info:
+                print info
                     
-                if count_train > 30000 or count_test > 30000: # upper limit of files count in a dir
-                    break
-            
+            if count > 30000 :
+                print 'Exceed the upper limit of a single file.'
+                break
+  
 	    if i % 100 == 99 :
 	        print 'have processed ',i+1,' files.'
-		
-    print 'items in train set: ',count_train
-    print 'items in test set: ',count_test
+	        
+	print 'items in train set: ',count
+	count = 0
+
+	for i in range(boundary,len(fnames)): # create test set
+	    
+	    info = create_sample(root,fnames[i-1],fnames[i],test_root)
+            if info:
+                print info
+                    
+            if count > 30000 :
+                print 'Exceed the upper limit of a single file.'
+                break
+  
+	    if i % 100 == 99 :
+	        print 'have processed ',i+1,' files.'
+          
+        print 'items in test set: ',count
